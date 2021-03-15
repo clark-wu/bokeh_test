19290b3bee7cdf784fcde2df7ff9d334e4952229
clark-wu <30484038+clark-wu@users.noreply.github.com>
2021-03-15 22:37:15 +0800

    添加react代码

---
+++ react_bokeh/front/client/app.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as Bokeh from "@bokeh/bokehjs"
+
+class App extends React.Component{
+    render(){
+        return (<h1>hello wrx2</h1>)
+    }
+}
+
+export default App;
---
+++ react_bokeh/front/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './client/app'
+
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
